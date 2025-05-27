9aa6cbaffa98b2ac66ff4230ec2030d66e1f2e28
Alexey <shustikov.alexey@gmail.com>
2025-05-27 14:25:39 +0300

    Third

--- app/src/main/java/hexlet/code/Router.java
+++ app/src/main/java/hexlet/code/Router.java
@@ -1,29 +1,70 @@
 package hexlet.code;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 public class Router {
-    public static Map<String, String> serve(List<Map<String, Object>> routes, String request) {
-        return routes.stream()
-                .filter(item -> match((String) item.get("path"), request))
-                .map(item -> item.get("handler"))
-                .map(handler -> (Map<String, String>) handler)
-                .findFirst().orElseThrow();
+    public static Map<String, Object> serve(List<Map<String, Object>> routes, String request) {
+        var root = buildPrefixTree(routes);
+        return getHandler(root, request);
     }
 
-    private static boolean match(String route, String request) {
-
-        var routArray = route.split("/");
+    private static Map<String, Object> getHandler(PrefixTreeNode root, String request) {
         var requestArray = request.split("/");
-        if (routArray.length == requestArray.length) {
-            return IntStream.range(0, requestArray.length)
-                    .mapToObj(index -> (routArray[index].startsWith(":"))
-                            || routArray[index].equals(requestArray[index]))
-                    .allMatch(item -> item);
-        } else {
-            return false;
+        var node = root;
+        for (var requestItem : requestArray) {
+            if (node.children.size() == 1) {
+                var childKey = node.children.keySet().iterator().next();
+                if (childKey.startsWith(":")) {
+                    node = node.children.get(childKey);
+                    continue;
+                }
+            }
+            node = node.children.get(requestItem);
+        }
+        return node.handler;
+    }
+
+    private static PrefixTreeNode buildPrefixTree(List<Map<String, Object>> routes) {
+        var root = new PrefixTreeNode("", null);
+        for (var routeRule : routes) {
+            var node = root;
+            var routArray = ((String) routeRule.get("path")).split("/");
+            for (var routeItem : routArray) {
+                if (routeItem.startsWith(":")) {
+                    routeItem = ":param";
+                }
+                if (!node.children.isEmpty() && node.children.containsKey(routeItem)) {
+                    node = node.children.get(routeItem);
+                } else {
+                    node = node.addNode(routeItem, null);
+                }
+            }
+            node.setHandler((Map<String, Object>) routeRule.get("handler"));
+        }
+        return root;
+    }
+
+    private static class PrefixTreeNode {
+        private final String value;
+        private HashMap<String, PrefixTreeNode> children;
+        private Map<String, Object> handler;
+
+        PrefixTreeNode(String value, Map<String, Object> handler) {
+            this.handler = handler;
+            this.value = value;
+            this.children = new HashMap<>();
+        }
+
+        public PrefixTreeNode addNode(String value, Map<String, Object> handler) {
+            var node = new PrefixTreeNode(value, handler);
+            this.children.put(value, node);
+            return node;
+        }
+
+        public void setHandler(Map<String, Object> handler) {
+            this.handler = handler;
         }
     }
 }
