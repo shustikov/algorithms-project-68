9bdbe4ec13fe0a83c1014ab1a7148ea55c720e4e
Alexey <shustikov.alexey@gmail.com>
2025-05-28 20:17:18 +0300

    Check

---
+++ app/src/main/java/hexlet/code/router/Router.java
@@ -0,0 +1,105 @@
+package hexlet.code.router;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+
+public class Router {
+    public static Map<String, String> serve(List<Map<String, Object>> routes, Map<String, String> request) {
+        var path = request.get("path");
+        var method = request.getOrDefault("method", "GET");
+        var root = buildPrefixTree(routes);
+        var result = new HashMap<String, Object>();
+        result.put("path", path);
+        result.put("method", method);
+        result.putAll(getHandler(root, path, method));
+        return (Map<String, String>) result.get("handler");
+    }
+
+    private static Map<String, Object> getHandler(PrefixTreeNode root, String request, String method) {
+        var requestArray = ("/" + request).split("/");
+        return explore(root, requestArray, method).getFirst();
+    }
+
+    private static List<HashMap<String, Object>> explore(PrefixTreeNode node, String[] requestArray, String method) {
+        var res = new ArrayList<HashMap<String, Object>>();
+        var element = new HashMap<String, Object>();
+        if (requestArray.length > 1) {
+            var newRequestArray = Arrays.copyOfRange(requestArray, 1, requestArray.length);
+            if (node.children.containsKey(newRequestArray[0])) {
+                res.addAll(explore(node.children.get(newRequestArray[0]), newRequestArray, method));
+            } else {
+                var paramsValues = node.children.keySet().stream().filter(ch -> ch.startsWith(":")).toList();
+                var items = paramsValues.stream().map(key -> node.children.get(key))
+                        .filter(paramNode -> Pattern.matches(paramNode.constrain == null ? newRequestArray[0] : paramNode.constrain, newRequestArray[0]))
+                        .map(n -> explore(n, newRequestArray, method))
+                        .filter(out -> !out.isEmpty())
+                        .flatMap(Collection::stream)
+                        .map(out -> addParamToMap(out, node.value.replace(":", ""), requestArray[0]))
+                        .toList();
+                res.addAll(items);
+            }
+        } else {
+            if (node.handler.containsKey(method)) {
+                element.put("handler", node.handler.get(method));
+                res.add(element);
+            }
+        }
+        return res;
+    }
+
+    private static HashMap<String, Object> addParamToMap(Map<String, Object> map, String name, String value) {
+        var params = map.getOrDefault("params", new HashMap<String, String>());
+        ((HashMap<String, String>) params).put(name, value);
+        map.put("params", params);
+        return (HashMap<String, Object>) map;
+    }
+
+    private static PrefixTreeNode buildPrefixTree(List<Map<String, Object>> routes) {
+        var root = new PrefixTreeNode("");
+        for (var routeRule : routes) {
+            var node = root;
+            var constrains = (Map<String, String>) routeRule.getOrDefault("constraints", Map.of());
+            var routArray = ((String) routeRule.get("path")).split("/");
+            for (var routeItem : routArray) {
+                if (!node.children.isEmpty() && node.children.containsKey(routeItem)) {
+                    node = node.children.get(routeItem);
+                } else {
+                    node = node.addNode(routeItem);
+                }
+                if (routeItem.startsWith(":") && constrains.size() > 0) {
+                    node.setConstrain(constrains.getOrDefault(routeItem.replace(":", ""), routeItem));
+                }
+            }
+            node.setHandler((String) routeRule.get("method"), (Map<String, Object>) routeRule.getOrDefault("handler", "GET"));
+        }
+        return root;
+    }
+
+    private static class PrefixTreeNode {
+        private final String value;
+        private HashMap<String, PrefixTreeNode> children;
+        private Map<String, Object> handler;
+        private String constrain;
+
+        PrefixTreeNode(String value) {
+            this.children = new HashMap<>();
+            this.handler = new HashMap<>();
+            this.value = value;
+        }
+
+        public PrefixTreeNode addNode(String value) {
+            var node = new PrefixTreeNode(value);
+            this.children.put(node.value, node);
+            return node;
+        }
+
+        public void setHandler(String method, Map<String, Object> handler) {
+            this.handler.put(method, handler);
+        }
+
+        public void setConstrain(String constrain) {
+            this.constrain = constrain;
+        }
+    }
+}
---
+++ app/src/test/java/hexlet/code/router/RouterTest.java
@@ -0,0 +1,96 @@
+package hexlet.code.router;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RouterTest {
+    private static final List<Map<String, Object>> routes = List.of(
+            Map.of(
+                    "method", "GET",
+                    "path", "/courses/:id",
+                    "handler", Map.of("body", "course!")
+            ),
+            Map.of(
+                    "method", "POST",
+                    "path", "/courses",
+                    "handler", Map.of("body", "created!")
+            ),
+            Map.of(
+                    "method", "GET",
+                    "path", "/courses/:course_id/exercises/:id",
+                    "handler", Map.of("body", "exercise!"),
+                    "constraints", Map.of("id", "\\d+", "course_id", "^[a-z]+$")
+            )
+    );
+
+    private static final List<Map<String, Object>> routesMain = List.of(
+            Map.of(
+                    "method", "POST",
+                    "path", "users/long/:id",
+                    "handler", Map.of("body", "handler1"),
+                    "constraints", Map.of("id", "\\d+")
+            ),
+            Map.of(
+                    "path", "users/long/:way",
+                    "handler", Map.of("body", "handler2"),
+                    "constraints", Map.of("way", "[a-z]")
+            ),
+            Map.of(
+                    "path", "users/long/way/:name",
+                    "handler", Map.of("body", "handler3"),
+                    "constraints", Map.of("name", "[a-z]+")
+            ),
+            Map.of(
+                    "path", "api/:id/:name/risc-v",
+                    "handler", Map.of("body", "handler4"),
+                    "constraints", Map.of("id", ".", "name", "^[a-z]+$")
+            ),
+            Map.of(
+                    "method", "PUT",
+                    "path", "api/:id/:uid",
+                    "handler", Map.of("body", "handler5")
+            ),
+            Map.of(
+                    "path", "api/to/Japan/",
+                    "handler", Map.of("body", "handler6")
+            ),
+            Map.of(
+                    "path", "/",
+                    "handler", Map.of("body", "root")
+            ),
+            Map.of(
+                    "path", "/courses/:course_id/exercises/:id",
+                    "handler", Map.of("body", "exercise!"),
+                    "constraints", Map.of("id", "\\d+", "course_id", "^[a-z]+$")
+            )
+    );
+
+
+    @Test
+    void serve() {
+        var request = Map.of("path", "/courses", "method", "POST");
+        var res = Router.serve(routes, request);
+
+        assertEquals("created!", ((Map) res.get("handler")).get("body"));
+    }
+
+    @Test
+    void serve_withConstraint() {
+        var request = Map.of("path", "/courses/js/exercises/1", "method", "GET");
+
+        var res = Router.serve(routes, request);
+
+        assertEquals("exercise!", ((Map) res.get("handler")).get("body"));
+    }
+
+    @Test
+    void serve_main() {
+        var request = Map.of("path", "/courses/js/exercises/1", "method", "GET");
+
+        var res = Router.serve(routesMain, request);
+    }
+}
